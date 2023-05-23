9b1966731cb1330ac6f1adac3c3bf1d8992af2e9
ipselon <apustovalov@gmail.com>
2023-05-23 23:30:51 +0300

    Update

--- schema/blocks/authorsBylinesBlock.ts
+++ schema/blocks/authorsBylinesBlock.ts
@@ -2,7 +2,7 @@ import {BlockConfig} from '@sitebud/sdk-lib';
 
 export const authorsBylinesBlock: BlockConfig = {
     label: 'Authors Bylines Block',
-    description: 'A brief section that attributes the content to its creator by displaying the author\'s name. It often appears near the beginning or end of an article or post and may include a link to the author\'s profile. The byline acknowledges the writer\'s work and offers readers an opportunity to explore more about the author and their other contributions.',
+    description: 'A brief section that attributes the content to its creator by displaying the author\'s name. It often appears near the beginning or end of an article or post and may include a link to the author\'s profile.',
     imageName: 'authors_bylines_block.png',
     components: {
         authorsBylines: {
