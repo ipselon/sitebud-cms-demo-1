65abce8891fdd4a8155dfd577495227e4d7686a8
ipselon <apustovalov@gmail.com>
2023-05-23 23:44:36 +0300

    Update

--- schema/blocks/tagsListBlock.ts
+++ schema/blocks/tagsListBlock.ts
@@ -2,7 +2,7 @@ import {BlockConfig} from '@sitebud/sdk-lib';
 
 export const tagsListBlock: BlockConfig = {
     label: 'Tags List Block',
-    description: 'A collection of keyword phrases that categorize and summarize the content of the webpage. Each tag is a link that, when clicked, takes you to a page showing all content associated with that tag. This makes it easier for visitors to find related content based on their interests.',
+    description: 'A collection of keyword phrases that categorize and summarize the content of the webpage. Each tag is a link that, when clicked, takes you to a page showing all content associated with that tag.',
     imageName: 'tags_list_block.png',
     components: {
         tags: {
